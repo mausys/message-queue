31b682bf2c8e9b246395fda0dc89c310f558605f
Simon Maurer <mail@maurer.systems>
2025-04-16 12:29:57 +0200

    update index.h

--- index.h
+++ index.h
@@ -1,15 +1,18 @@
 #pragma once
 
-#include <stdint.h>
+
+#include <limits.h>
 #include <stdatomic.h>
 
-typedef uint32_t index_t;
+typedef unsigned int index_t;
 typedef atomic_uint atomic_index_t;
 
-#define INDEX_END UINT32_MAX
+#define INDEX_END UINT_MAX
 
-#define CONSUMED_FLAG ((index_t)1 << 31)
+#define CONSUMED_FLAG ((index_t)1 << (UINT_WIDTH - 1))
 
 #define ORIGIN_MASK CONSUMED_FLAG
 
 #define INDEX_MASK (~ORIGIN_MASK)
+
+
