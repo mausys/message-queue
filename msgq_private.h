dbd2a968051ae37558e3585afde9900668b7ad42
Simon Maurer <mail@maurer.systems>
2025-10-07 19:24:37 +0200

    remove everything except spin/modex code

--- msgq_private.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "msgq.h"
-
-#include "index.h"
-
-index_t producer_get_current(producer_t *producer);
-index_t consumer_get_current(consumer_t *consumer);
-index_t producer_get_overrun(producer_t *producer);
